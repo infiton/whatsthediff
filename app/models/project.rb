8ee30b0b1edb048cabeea848b2fa6abe0bdc573a
Kyle Tate <kbt.tate@gmail.com>
2014-11-13 21:00:36 -0500

    fix state attribute on project

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,7 +3,7 @@ class Project < ActiveRecord::Base
   obfuscate_id :spin => 8675309
   belongs_to :user
 
-  validates_presence_of :state, :in => [:new, :source_uploaded, :target_added, :target_uploaded, :data_uploaded, :complete]
+  validates :state, inclusion: {in: ["new", "source_uploaded", "target_added", "target_uploaded", "data_uploaded", "complete"] }
   #need to remove data_id and target_data_id from schema
   def project_data
     ProjectData.where(project_id: self.id).first
@@ -14,32 +14,32 @@ class Project < ActiveRecord::Base
   end
 
   def source_uploaded
-    self.state = :source_uploaded
+    self.state = "source_uploaded"
     self.save
   end
 
   def target_added
-    self.state = :target_added
+    self.state = "target_added"
     self.save
   end
 
   def target_uploaded
-    self.state = :target_uploaded
+    self.state = "target_uploaded"
     self.save
   end
 
   def data_uploaded
-    self.state = :data_uploaded
+    self.state = "data_uploaded"
     self.save
   end
 
   def complete
-    self.state = :complete
+    self.state = "complete"
     self.save
   end
 
   def complete?
-    self.state.to_s == "complete"
+    self.state == "complete"
   end
 
   def add_target_user user
