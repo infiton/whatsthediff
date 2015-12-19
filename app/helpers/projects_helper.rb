697f1b2860b60e35dab859829fd26e8356300656
infiton <kbt.tate@gmail.com>
2015-12-18 19:27:09 -0800

    remove v1 code

--- app/helpers/projects_helper.rb
+++
@@ -1,9 +0,0 @@
-module ProjectsHelper
-	def get_csv project, list
-		link_to "get csv", download_project_path(project, :list => list, :format => "csv")
-	end
-
-	def get_xls project, list
-		link_to "get xls", download_project_path(project, :list => list, :format => "xls")
-	end
-end
--- db/migrate/20141109190944_change_base_user_column_name.rb
+++
@@ -1,6 +0,0 @@
-class ChangeBaseUserColumnName < ActiveRecord::Migration
-  def change
-  	rename_column :projects, :base_user_id, :user_id
-  	rename_column :projects, :base_data_id, :data_id
-  end
-end
